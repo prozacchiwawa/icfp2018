4367a417632b5ea79fe93fd50f912e713adda96a
arty <art.yerkes@gmail.com>
2018-07-23 04:20:56 -0700

    Starting to get results

--- doit.sh
+++ doit.sh
@@ -3,5 +3,5 @@
 prefix="$1"
 for i in 0 1 2 3 4 5 6 7 8 9 ; do
     echo "model ${prefix}$i"
-    ./run.sh skeleton ./problems/F/FA${prefix}$i_tgt.mdl ./res/FA${prefix}$i.nbt
+    ./run.sh skeleton ./problems/F/FA${prefix}${i}_tgt.mdl ./res/FA${prefix}${i}.nbt
 done
